3666782bfbdfa65ee8084e86f038097e83f99cfa
Matt Manning <matt.manning@gmail.com>
2019-12-04 00:38:44 -0500

    use array combination method

--- 04/a.rb
+++ 04/a.rb
@@ -5,8 +5,7 @@ count = 0
 
 min.upto(max) do |n|
   arr = n.to_s.split('').map(&:to_i)
-  if (arr.sort == arr) &&
-    ((arr[0] == arr[1]) || (arr[1] == arr[2]) || (arr[2] == arr[3]) || (arr[3] == arr[4]) || (arr[4] == arr[5]))
+  if (arr.sort == arr) && arr.combination(2).any? { |c| c.uniq.length == 1 }
     count += 1
   end
 end
