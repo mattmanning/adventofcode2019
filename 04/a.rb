9a3e686249b5a031b2817bd92b2a301992479e15
Matt Manning <matt.manning@gmail.com>
2019-12-04 01:13:50 -0500

    just use uniq

--- 04/a.rb
+++ 04/a.rb
@@ -6,8 +6,7 @@ count = 0
 min.upto(max) do |n|
   arr = n.to_s.split('').map(&:to_i)
   if (arr.sort == arr)
-    group = arr.group_by {|x| x}.values
-    if group.any? { |v| v.length > 1 }
+    if arr.uniq.length < arr.length
       count += 1
     end
   end
