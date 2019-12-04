1048a4f4141638458dfbdc0ee60875a03121b955
Matt Manning <matt.manning@gmail.com>
2019-12-04 00:42:08 -0500

    just use group

--- 04/a.rb
+++ 04/a.rb
@@ -5,8 +5,11 @@ count = 0
 
 min.upto(max) do |n|
   arr = n.to_s.split('').map(&:to_i)
-  if (arr.sort == arr) && arr.combination(2).any? { |c| c.uniq.length == 1 }
-    count += 1
+  if (arr.sort == arr)
+    group = arr.group_by {|x| x}.values
+    if group.any? { |v| v.length > 1 }
+      count += 1
+    end
   end
 end
 
